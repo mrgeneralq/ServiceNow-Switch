8f2e3f30800dddfe4c292e4cf46c3820bf071ec1
Quinten <mrgeneralq@gmail.com>
2021-05-27 22:47:49 +0200

    Working navigation

--- src/content-scripts/buttons.js
+++ src/content-scripts/buttons.js
@@ -1,21 +1,48 @@
 (async ()=> {
+    loadButtons();
+})();
 
+chrome.runtime.onMessage.addListener(async(message, sender, sendResponse) => {
+    
+    if(message.action == "refresh-buttons"){
+
+    }
+
+});
+
+async function loadButtons(){
     const urlDomain = location.host.split('.')[0];
     const instance = await InstanceService.getInstanceByPrefix(urlDomain);
 
     const instances = await InstanceService.getAllInstancesByGroup(instance.instanceGroup);
+    const buttonToolbar = document.createElement("div");
+    buttonToolbar.setAttribute("id", "switch-toolbar");
+
     for(var instanceItem of instances){
-      //  alert(instanceItem.label);
+        const button = document.createElement("button");
+        button.innerHTML = instanceItem.label;
+        button.setAttribute("data-prefix", instanceItem.prefix);
+
+        button.addEventListener("click", function(event){
+            
+            var instancePrefix = event.target.dataset.prefix;
+            navigateToInstance(instancePrefix);
+
+        });
+        
+        buttonToolbar.appendChild(button);
     }
 
-    chrome.runtime.onMessage.addListener(async(message, sender, sendResponse) => {
-        console.log("aaa",message);
+    const body = document.querySelector("body");
+    body.appendChild(buttonToolbar);
+}
 
-        if(message.action == "refresh-buttons"){
-            alert("refresh buttons!");
-        }
+function navigateToInstance(instancePrefix){
 
-    });
+    var parameters = (location.pathname+location.search).substr(1);
+    var beginUrl = window.location.protocol + "//";
+    var navUrl = beginUrl + instancePrefix + ".service-now.com/" + parameters;
 
+    location.href = navUrl;
 
-})();
+}
